406c96f0d96b5b3aef22be6c69428527d4ad35f4
ruvialexandra <ruviaranareyes@gmail.com>
2024-06-20 03:32:27 -0500

    Update Galeria.js

--- src/component/Galeria.js
+++ src/component/Galeria.js
@@ -27,7 +27,7 @@ function Galeria() {
                     <div className="row g-0">
                         <div className="col-md-4">
                             <div className="card-body">
-                                <h5 className="card-title">Conoce el interior de nuestra sede: : Un Recorrido Virtual</h5>
+                                <h5 className="card-title">Conoce el interior de nuestra sede: Un Recorrido Virtual</h5>
                                 <hr class="border border-primary border-3 opacity-75"></hr>
                                 <p className="card-text"> ¡Acompáñanos en este recorrido virtual y sumérgete en la experiencia única de conocer el interior de nuestra sede!</p>
                                 <p className="card-text">Sean bienvenidos a un recorrido exclusivo por el interior de nuestra sede. En este video, te invitamos a explorar cada rincón de nuestras instalaciones en San Miguel, Lima - Perú. Desde la recepción, donde serás recibido con calidez, hasta nuestras áreas especializadas donde ocurre la magia del diagnóstico dental avanzado.</p>
