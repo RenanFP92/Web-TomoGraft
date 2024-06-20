e0fe3f886a4751841be7848231a7add44ec6ba27
ruvialexandra <ruviaranareyes@gmail.com>
2024-06-20 01:07:01 -0500

    card listo falta editar

--- src/component/Conocenos.js
+++ src/component/Conocenos.js
@@ -1,37 +1,53 @@
 import Imagen from '../Imagen';
 function Conocenos(){
     return(
-    <div>
-        <h2 id="conocenos">ACERCA DE TOMOGRAFT</h2>
-        <div className="card mb-3">
-          <div className="row g-0">
-            <div className="col-md-6">
-              <img src={Imagen.fotoprincipal} className="img-fluid rounded" alt="foto principal de la pagina" />
-            </div>
-            <div className="col-md-6">
-              <div className="card-body">
-                <p className="card-text">
-                TOMOGRAFT tiene como principal propósito brindar el mejor diagnóstico a través de imágenes de alta definición y servicio de laboratorio dental para pacientes odontológicos que desean mejorar su salud bucal y disfrutar de una mejor calidad de vida.
-                </p>
-                <hr></hr>
-                <h5 className="card-title">DIAGNÓSTICO DE IMAGENES DENTALES</h5>
-                <p className="card-text">
-                  <li>Radiografías dentales 2D </li>
-                  <li>Tomografías dentales 3D</li>
+    <div >
 
-                </p>
-                <hr></hr>
-                <h5 className="card-title">LABORATORIO DENTAL</h5>
-                <p className="card-text">
-                  <li>Prótesis</li>
-                  <li>Coronas: Metal y Zirconio</li>
-                  <li>Carillas</li>
-                  <li>Aparatos de ortodoncia</li>
-                </p>
-              </div>
-            </div>
+    <div className="card mb-3" style={{ maxwidth: '540px' }} >
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={Imagen.fotoprincipal}  className="img-fluid rounded-start" alt="..."/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
           </div>
         </div>
+      </div>
+    </div>
+
+    <div className="card mb-3" style={{ maxwidth: '540px' }} >
+      <div className="row g-0">
+        <div className="col-md-4"> 
+          <img src={Imagen} className="img-fluid rounded-start" alt="..."/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="card mb-3" style={{ maxwidth: '540px' }}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={Imagen} className="img-fluid rounded-start" alt="..."/>
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+       
     </div>
     )
 }
