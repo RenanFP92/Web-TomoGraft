34c8a8025c2b8de60ebce3aec8f5d051de4f9b8d
RenanFP92 <138515115+RenanFP92@users.noreply.github.com>
2024-06-19 21:31:38 -0500

    UPDATE

--- src/component/MenuPrincipal.js
+++ src/component/MenuPrincipal.js
@@ -3,29 +3,29 @@ function MenuPrincipal() {
     return(
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#38a1b2' }}  id="menuprincipal">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">TomoGraft</a>
+            <a className="navbar-brand" href="#" style={{ color: 'White ' , fontSize: '30px'  }}>TomoGraft</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#conocenos">¿Quiénes somos?</a>
+                            <a className="nav-link active" aria-current="page" href="#conocenos" style={{ color: 'White ' , fontSize: '19px'  }}>¿Quiénes somos?</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#servicios">Servicios y Tecnología</a>
+                            <a className="nav-link" href="#servicios" style={{ color: 'White ' , fontSize: '19px'  }}>Servicios y Tecnología</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#testimonios">Testimonios</a>
+                            <a className="nav-link" href="#testimonios" style={{ color: 'White ' , fontSize: '19px'  }}>Testimonios</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#faq">Preguntas Frecuentes</a>
+                            <a className="nav-link" href="#faq" style={{ color: 'White ' , fontSize: '19px'  }}>Preguntas Frecuentes</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="">Ubícanos</a>
+                            <a className="nav-link" href="" style={{ color: 'White ' , fontSize: '19px'  }}>Ubícanos</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="">Contáctanos</a>
+                            <a className="nav-link" href="" style={{ color: 'White ' , fontSize: '19px'  }}>Contáctanos</a>
                         </li>
                     </ul>
                 </div>
